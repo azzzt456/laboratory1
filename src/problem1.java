affb5ce5698d0612def19c86107088e9b0fb1787
adelt <231592@astanait.edu.kz>
2024-04-06 10:32:13 +0500

    10prob

---
+++ src/problem1.java
@@ -0,0 +1,10 @@
+import jdk.nashorn.internal.parser.Scanner;
+
+public class problem1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.()
+
+    }
+
+}
